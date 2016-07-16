c5a8218a231a5d0a8dd98cdcff1b0a34a732b82c
Pavel Bondar <pbondar@infoblox.com>
2016-07-16 13:47:06 +0300

    Make storage thread-safe
    Use mutex to synchronize set/get/delete operations on map.

--- storage/map.go
+++ storage/map.go
@@ -1,27 +1,39 @@
 package storage
 
+import (
+	"sync"
+)
+
 type Storage struct {
 	Map map[string]string
+	Mutex *sync.Mutex
 }
 
 func (s *Storage) Init() {
 	s.Map = make(map[string]string)
+	s.Mutex = &sync.Mutex{}
 }
 
 func (s *Storage) Set(key, value string) {
+	s.Mutex.Lock()
 	s.Map[key] = value
+	s.Mutex.Unlock()
 }
 
 func (s *Storage) Get(key string) (string, bool) {
+	s.Mutex.Lock()
 	value, ok := s.Map[key]
+	s.Mutex.Unlock()
 	return value, ok
 }
 
-func (s *Storage) Delete(key string) bool {
+func (s *Storage) Delete(key string) (deleted bool) {
+	s.Mutex.Lock()
 	_, exists := s.Map[key]
 	if exists == true {
 		delete(s.Map, key)
-		return true
+		deleted = true
 	}
-	return false
+	s.Mutex.Unlock()
+	return
 }
