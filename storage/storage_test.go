0c5f9599e86f4914e0a40362b8cbb1c92437fdf9
Pavel Bondar <pbondar@infoblox.com>
2016-08-07 20:54:40 +0300

    Add more tests for storage

--- storage/storage_test.go
+++ storage/storage_test.go
@@ -41,3 +41,38 @@ func TestDelNotExistent(t *testing.T) {
 	ok := s.del("noKey")
 	assert.Equal(t, false, ok)
 }
+
+
+func TestQueryGet(t *testing.T) {
+	s := initStorageWithData()
+	cmd := Command{Key: KEY}
+	reply := s.Query("get", cmd)
+	assert.Equal(t, true, reply.Exists)
+	assert.Equal(t, VAL, reply.Value)
+	assert.Equal(t, KEY, reply.Key)
+}
+
+func TestQuerySetGet(t *testing.T) {
+	s := initStorageWithData()
+	setCmd := Command{Key: "newKey", Value: "newVal"}
+	s.Query("set", setCmd)
+	getCmd := Command{Key: "newKey"}
+	reply := s.Query("get", getCmd)
+	assert.Equal(t, true, reply.Exists)
+	assert.Equal(t, "newVal", reply.Value)
+	assert.Equal(t, "newKey", reply.Key)
+}
+
+func TestQueryDel(t *testing.T) {
+	s := initStorageWithData()
+	delCmd := Command{Key: KEY}
+	reply := s.Query("del", delCmd)
+	assert.Equal(t, true, reply.Exists)
+}
+
+func TestQueryDelNotExistent(t *testing.T) {
+	s:= initStorageWithData()
+	delCmd := Command{Key: "NonExistent"}
+	reply := s.Query("del", delCmd)
+	assert.Equal(t, false, reply.Exists)
+}
